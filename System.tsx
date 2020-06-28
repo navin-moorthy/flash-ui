7849a19e1d728a066ae611e633f51d965d3f16d0
Navin <navin007.a@gmail.com>
2020-06-28 07:38:20 +0530

    chore: :fire: removing a file thats not needed

--- System.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react"
-import { useSystem, SystemProps } from "./System.hook"
-
-export function System(props: SystemProps) {
-  const hook = useSystem(props)
-  return <div>This is a System component</div>
-}
-
-export default System
